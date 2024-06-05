294adc238c2691f298f823543b4eed977997d985
Aniket Sharma <sharmaaniket5172@gmail.com>
2024-06-05 16:35:38 +0530

    all done

---
+++ script.js
@@ -0,0 +1,26 @@
+const token = "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"
+const inputTxt = document.getElementById("input")
+const image = document.getElementById("image")
+const button = document.getElementById("btn")
+
+
+async function query() {
+  image.src = "/loading.gif"
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/Melonie/text_to_image_finetuned",
+		{
+			headers: { Authorization: `Bearer ${token}` },
+			method: "POST",
+			body: JSON.stringify({"inputs": inputTxt.value}),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+
+button.addEventListener('click',async function (){
+  query().then((response) => {
+    const objectURL = URL.createObjectURL(response)
+    image.src = objectURL
+  });
+})
